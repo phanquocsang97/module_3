61b075409c34056596d377f70462c2b3126d2352
phanquocsang97 <quocsang0310@gmail.com>
2023-08-10 11:26:59 +0700

    done homework

--- ss1_database/ss1_exercise/exercise.sql
+++ ss1_database/ss1_exercise/exercise.sql
@@ -1,11 +1,25 @@
-use demo;
+use ss1_exercise;
+
 create table Class(
-id int,
-`name` varchar(200)
+id int primary key auto_increment,
+`name` varchar(45)
 );
+
 create table Teacher(
-id int,
+id int primary key auto_increment,
 `name` varchar(200),
 age int,
 country varchar(50)
 );
+
+insert into Class(`name`)
+values ("Sang"),("Lam"),("Viet");
+
+insert into Teacher(`name`,age,country)
+values ("Chanh",35,"VN"),("Trung",30,"VN"),("Cong",32,"VN");
+
+select *
+from Class;
+
+select *
+from Teacher;
