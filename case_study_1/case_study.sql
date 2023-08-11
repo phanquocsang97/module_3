13c5c6217172840ee5561fb64b2dd4dd98968cb4
phanquocsang97 <quocsang0310@gmail.com>
2023-08-11 15:12:00 +0700

    done homework

--- case_study_1/case_study.sql
+++ case_study_1/case_study.sql
@@ -15,16 +15,16 @@ ten_bo_phan varchar(45)
 );
 create table nhan_vien(
 ma_nhan_vien int primary key auto_increment,
-ho_ten varchar(45),
-ngay_sinh date,
-so_cmnd varchar(45),
-luong double,
-so_dien_thoai varchar(45),
+ho_ten varchar(45) not null,
+ngay_sinh date not null,
+so_cmnd varchar(45) not null,
+luong double not null,
+so_dien_thoai varchar(45) not null,
 email varchar(45),
 dia_chi varchar(45),
-ma_vi_tri int ,foreign key(ma_vi_tri) references vi_tri(ma_vi_tri),
-ma_trinh_do int, foreign key(ma_trinh_do) references trinh_do(ma_trinh_do),
-ma_bo_phan int, foreign key(ma_bo_phan) references bo_phan(ma_bo_phan)
+ma_vi_tri int not null ,foreign key(ma_vi_tri) references vi_tri(ma_vi_tri),
+ma_trinh_do int not null, foreign key(ma_trinh_do) references trinh_do(ma_trinh_do),
+ma_bo_phan int not null, foreign key(ma_bo_phan) references bo_phan(ma_bo_phan)
 );
 create table loai_khach(
 ma_loai_khach int primary key auto_increment,
@@ -33,12 +33,12 @@ ten_loai_khach varchar(45)
 
 create table khach_hang(
 ma_khach_hang int primary key auto_increment,
-ma_loai_khach int, foreign key(ma_loai_khach) references loai_khach(ma_loai_khach),
-ho_ten varchar(45),
-ngay_sinh date,
-gioi_tinh bit(1),
-so_cmnd varchar(45),
-so_dien_thoai varchar(45),
+ma_loai_khach int not null, foreign key(ma_loai_khach) references loai_khach(ma_loai_khach),
+ho_ten varchar(45) not null,
+ngay_sinh date not null,
+gioi_tinh bit(1) not null,
+so_cmnd varchar(45) not null,
+so_dien_thoai varchar(45) not null,
 email varchar(45),
 dia_chi varchar(45)
 );
@@ -52,12 +52,12 @@ ten_loai_dich_vu varchar(45)
 );
 create table dich_vu(
 ma_dich_vu int primary key auto_increment,
-ten_dich_vu varchar(45),
+ten_dich_vu varchar(45) not null,
 dien_tich int,
-chi_phi_thue double,
+chi_phi_thue double not null,
 so_nguoi_toi_da int,
-ma_kieu_thue int, foreign key(ma_kieu_thue) references kieu_thue(ma_kieu_thue),
-ma_loai_dich_vu int, foreign key(ma_loai_dich_vu) references loai_dich_vu(ma_loai_dich_vu),
+ma_kieu_thue int not null, foreign key(ma_kieu_thue) references kieu_thue(ma_kieu_thue),
+ma_loai_dich_vu int not null, foreign key(ma_loai_dich_vu) references loai_dich_vu(ma_loai_dich_vu),
 tieu_chuan_phong varchar(45),
 mo_ta_tien_nghi_khac varchar(45),
 dien_tich_ho_boi double,
@@ -65,28 +65,28 @@ so_tang int
 );
 create table hop_dong(
 ma_hop_dong int primary key auto_increment,
-ngay_lam_hop_dong datetime,
-ngay_ket_thuc datetime,
-tien_dat_coc double,
-ma_nhan_vien int, foreign key(ma_nhan_vien) references nhan_vien(ma_nhan_vien),
-ma_khach_hang int, foreign key(ma_khach_hang) references khach_hang(ma_khach_hang),
-ma_dich_vu int, foreign key(ma_dich_vu) references dich_vu(ma_dich_vu)
+ngay_lam_hop_dong datetime not null,
+ngay_ket_thuc datetime not null,
+tien_dat_coc double not null,
+ma_nhan_vien int not null, foreign key(ma_nhan_vien) references nhan_vien(ma_nhan_vien),
+ma_khach_hang int not null, foreign key(ma_khach_hang) references khach_hang(ma_khach_hang),
+ma_dich_vu int not null, foreign key(ma_dich_vu) references dich_vu(ma_dich_vu)
 );
 create table dich_vu_di_kem(
 ma_dich_vu_di_kem int primary key auto_increment,
-ten_dich_vu_di_kem varchar(45),
-gia double,
-don_vi varchar(10),
+ten_dich_vu_di_kem varchar(45) not null,
+gia double not null,
+don_vi varchar(10) not null,
 trang_thai varchar(45)
 );
 create table hop_dong_chi_tiet(
 ma_hop_dong_chi_tiet int primary key auto_increment,
-ma_hop_dong int, foreign key(ma_hop_dong) references hop_dong(ma_hop_dong),
-ma_dich_vu_di_kem int, foreign key(ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem),
-so_luong int
+ma_hop_dong int not null, foreign key(ma_hop_dong) references hop_dong(ma_hop_dong),
+ma_dich_vu_di_kem int not null, foreign key(ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem),
+so_luong int not null
 );
 
-/* Câu 1 */
+/* Câu 1. Thêm mới thông tin cho tất cả các bảng có trong CSDL để có thể thỏa mãn các yêu cầu bên dưới */
 
 insert into vi_tri(ten_vi_tri)
 values ("Quản lí"),("Nhân viên");
@@ -180,12 +180,13 @@ values
 (2,1,2),
 (2,12,2);
 
-/* Câu 2*/
+/* Câu 2. Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các kí tự "H", "T", "K" và có tối đa 15 kí tự */
 
 select * from nhan_vien
 where ho_ten like "T%" or ho_ten like "H%" or ho_ten like "K%" and length(ho_ten) <= 15;
 
-/* Câu 3*/
+/* Câu 3. Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở "Đà Nẵng" hoặc "Quảng Trị" */
+
 
 
 
