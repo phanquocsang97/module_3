cce87951f0984d7aa158feed3a1db0e876b98fac
phanquocsang97 <quocsang0310@gmail.com>
2023-08-16 08:09:37 +0700

    done homework

--- case_study_1/case_study.sql
+++ case_study_1/case_study.sql
@@ -181,19 +181,29 @@ values
 (2,12,2);
 
 /* Câu 2. Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các kí tự "H", "T", "K" và có tối đa 15 kí tự */
+-- Hiển thị nhân viên
+-- Tên bắt đầu bằng H,T,K
 
 select * from nhan_vien
 where ho_ten like "T%" or ho_ten like "H%" or ho_ten like "K%" and length(ho_ten) <= 15;
 
 /* Câu 3. Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở "Đà Nẵng" hoặc "Quảng Trị" */
+-- Hiển thị khách hàng
+-- Tuổi 18-> 50
+-- Ở ĐN or QT
+
 select * from khach_hang
 where (select datediff(curdate(),ngay_sinh)/365.25 between 18 and 50)
 and dia_chi like "%Đà Nẵng" or dia_chi like "%Quảng Trị";
 
 /* Câu 4. Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng.
 Chỉ đếm những khách hàng nào có Tên loại khách hàng là "Diamond". */
+-- Đếm khách hàng đặt phòng bn lần
+-- Sắp xếp tăng dần theo số lần đặt phòng
+-- Chỉ đếm khách "Diamond"
 
-select khach_hang.ho_ten,count(hop_dong.ma_hop_dong) as "so_lan_dat_phong"
+select khach_hang.ho_ten,
+       count(hop_dong.ma_hop_dong) as "so_lan_dat_phong"
 from loai_khach
 join khach_hang
 on loai_khach.ma_loai_khach = khach_hang.ma_loai_khach
@@ -201,24 +211,22 @@ join hop_dong
 on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
 where loai_khach.ten_loai_khach = "Diamond"
 group by khach_hang.ma_khach_hang
-order by so_lan_dat_phong asc;
+order by so_lan_dat_phong;
 
 /* Câu 5. Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong,ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien (Với
 tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem,
 hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).*/
 
-
 -- set sql_mode = 1; 
 select khach_hang.ma_khach_hang,
-khach_hang.ho_ten,
-loai_khach.ten_loai_khach,
-hop_dong.ma_hop_dong,
-dich_vu.ten_dich_vu,
-hop_dong.ngay_lam_hop_dong,
-hop_dong.ngay_ket_thuc,
-if (hop_dong_chi_tiet.so_luong is null,
-dich_vu.chi_phi_thue,
-sum(dich_vu.chi_phi_thue + hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia)) as "tong_tien"
+       khach_hang.ho_ten,
+       loai_khach.ten_loai_khach,
+       hop_dong.ma_hop_dong,
+       dich_vu.ten_dich_vu,
+       hop_dong.ngay_lam_hop_dong,
+       hop_dong.ngay_ket_thuc,
+ifnull(sum(dich_vu.chi_phi_thue + hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia),
+dich_vu.chi_phi_thue) as "tong_tien"
 from khach_hang
 left join loai_khach 
 on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
@@ -231,14 +239,20 @@ on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
 left join dich_vu_di_kem
 on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
 group by hop_dong.ma_hop_dong;
-SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-
+-- SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
 
 
 /* Câu 6. Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được khách hàng
 thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3). */
+-- Hiển thị tất cả dịch vụ
+-- Hiển thị Dịch vụ đặt từ quý 1 năm 2021
+-- Hiển thị dịch vụ chưa được đặt từ quý 1 năm 2021
 
-select dich_vu.ma_dich_vu,dich_vu.ten_dich_vu,dich_vu.dien_tich,dich_vu.chi_phi_thue,loai_dich_vu.ten_loai_dich_vu
+select dich_vu.ma_dich_vu,
+       dich_vu.ten_dich_vu,
+       dich_vu.dien_tich,
+       dich_vu.chi_phi_thue,
+       loai_dich_vu.ten_loai_dich_vu
 from hop_dong
 join dich_vu
 on  dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
@@ -249,11 +263,123 @@ select dich_vu.ten_dich_vu
 from dich_vu 
 join hop_dong 
 on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
-where hop_dong.ngay_lam_hop_dong between "2021/01/01" and "2021/03/01")
+where hop_dong.ngay_lam_hop_dong between "2021/01/01" and "2021/03/31")
 group by ma_dich_vu
 order by chi_phi_thue desc;
 
+-- Câu 7. Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại
+-- dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
 
+select dich_vu.ma_dich_vu,
+       dich_vu.ten_dich_vu,
+       dich_vu.dien_tich,
+       dich_vu.so_nguoi_toi_da,
+       dich_vu.chi_phi_thue,
+       loai_dich_vu.ten_loai_dich_vu
+from dich_vu
+join loai_dich_vu
+on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+join hop_dong
+on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+where dich_vu.ten_dich_vu not in (
+select dich_vu.ten_dich_vu 
+from dich_vu
+join hop_dong
+on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+where hop_dong.ngay_lam_hop_dong between "2021/01/01" and "2021/12/31")
+group by dich_vu.ma_dich_vu;
+
+-- Câu 8. Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+
+-- Cách 1 :
+select khach_hang.ho_ten 
+from khach_hang
+union 
+select khach_hang.ho_ten
+from khach_hang;
+
+-- Cách 2:
+select distinct khach_hang.ho_ten
+from khach_hang;
+
+-- Cách 3:
+select khach_hang.ho_ten
+from khach_hang
+group by khach_hang.ho_ten;
+
+
+-- Câu 9. Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+
+select month(hop_dong.ngay_lam_hop_dong) as "thang",
+       count(hop_dong.ma_hop_dong) as "so_luong_khach_hang"
+from  hop_dong
+where hop_dong.ngay_lam_hop_dong between "2021/01/01" and "2021/12/31"
+group by month(hop_dong.ngay_lam_hop_dong)
+order by thang;
+
+-- Câu 10. Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. Kết quả hiển thị bao gồm ma_hop_dong,
+-- ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+
+select hop_dong.ma_hop_dong,
+       hop_dong.ngay_lam_hop_dong,
+       hop_dong.ngay_ket_thuc,
+       hop_dong.tien_dat_coc,
+       sum(ifnull(hop_dong_chi_tiet.so_luong,0)) as "so_luong_dich_vu_di_kem"
+from hop_dong
+left join hop_dong_chi_tiet
+on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+group by hop_dong.ma_hop_dong
+order by ma_hop_dong; 
+
+-- Câu 11. Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+
+select dich_vu_di_kem.ma_dich_vu_di_kem,
+       dich_vu_di_kem.ten_dich_vu_di_kem
+from loai_khach
+join khach_hang
+on loai_khach.ma_loai_khach = khach_hang.ma_loai_khach
+join hop_dong
+on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+join hop_dong_chi_tiet
+on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+join dich_vu_di_kem
+on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+where loai_khach.ten_loai_khach = "Diamond" and (khach_hang.dia_chi like "%Vinh%" or khach_hang.dia_chi like "%Quảng Ngãi%")
+order by dich_vu_di_kem.ma_dich_vu_di_kem;
+
+-- Câu 12. Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu,
+-- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ đã từng được
+-- khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+
+select hop_dong.ma_hop_dong,
+       nhan_vien.ho_ten as "ho_ten_nhan_vien",
+       khach_hang.ho_ten as "ho_ten_khach_hang",
+       khach_hang.so_dien_thoai,
+       hop_dong.ma_dich_vu,
+       dich_vu.ten_dich_vu,
+       sum(ifnull(hop_dong_chi_tiet.so_luong,0)) as "so_luong_dich_vu_di_kem",
+       hop_dong.tien_dat_coc
+from nhan_vien
+left join hop_dong 
+on nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
+left join khach_hang 
+on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+left join dich_vu 
+on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+left join hop_dong_chi_tiet 
+on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+left join dich_vu_di_kem 
+on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+where dich_vu.ma_dich_vu in (select hop_dong.ma_dich_vu
+from hop_dong
+where hop_dong.ngay_lam_hop_dong between "2020/10/01" and "2020/12/31")
+and dich_vu.ma_dich_vu not in 
+(select dich_vu.ma_dich_vu
+from dich_vu
+join hop_dong
+on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+where hop_dong.ngay_lam_hop_dong between "2021/01/01" and "2021/06/30") 
+group by hop_dong.ma_hop_dong;
 
 
 
